3e38d333b8df04b32fa91a72f76971fcd41e25ec
cbbarclay <cbbarclay@hotmail.com>
2014-02-11 15:49:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
+   fs = require('fs')
+   fs.readFile('/srv/www/app1/shared/config/app_data.yml', 'utf8', function (err,data) {
+     if (err) {
+       return console.log(err);
+     }
+     res.sendfile(data);
+   });
    res.sendfile('./index.html');
 });
 
