a15c581fd9c5c250d51724df0603c850179684b0
cbbarclay <cbbarclay@hotmail.com>
2014-02-11 17:01:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var app = express();
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.writeHead(200);
-    var stream = fs.createReadStream('/srv/www/app1/shared/config/app_data.yml');
-    util.pump(stream, res, function(error) {
-        res.end();
-        return;
-    }
+    res.sendfile('/srv/www/app1/shared/config/app_data.yml');
 });
 
 app.use(express.static('public'));
