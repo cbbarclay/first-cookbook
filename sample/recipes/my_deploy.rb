499c26a32aa1829921f5af22ff3a8b86da53e59c
cbbarclay <cbbarclay@hotmail.com>
2014-02-13 10:53:09 -0800

    Update my_deploy.rb

--- sample/recipes/my_deploy.rb
+++ sample/recipes/my_deploy.rb
@@ -2,27 +2,20 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
 
+
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
     path deploy[:deploy_to]
   end
 
+  opsworks_rails do
+    deploy_data deploy
+    app application
+  end
+
   opsworks_deploy do
     deploy_data deploy
     app application
-  end
-
-  opsworks_nodejs do
-    deploy_data deploy
-    app application
-  end
-
-  ruby_block "restart node.js application #{application}" do
-    block do
-      Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
-      Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
-      $? == 0
-    end
   end
 end
