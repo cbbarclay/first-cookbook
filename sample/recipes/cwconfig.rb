010e0fcdb9ee08273d8853b66c64fecf0b5bf691
cbbarclay <cbbarclay@hotmail.com>
2014-04-17 12:32:29 -0700

    Update cwconfig.rb

--- sample/recipes/cwconfig.rb
+++ sample/recipes/cwconfig.rb
@@ -3,5 +3,5 @@ file "/tmp/cwlogs" do
   group "root"
   mode "0755"
   content "[general]\n state_file = /tmp/watchstate\n[/var/log/kern.log]\n datetime_format = %b %d %H:%M:%S\nlog_group_name = /var/log/kern.log\nfile = /var/log/kern.log\nlog_stream_name = ::hostname::"
-  action : create_if_missing
+  action :create_if_missing
 end
