d0ef116a88ac4d5a81971f164cca0bbd6e39d447
cbbarclay <cbbarclay@hotmail.com>
2014-04-17 12:05:49 -0700

    Create cwconfig.rb

---
+++ sample/recipes/cwconfig.rb
@@ -0,0 +1,7 @@
+file "/tmp/cwlogs" do
+  owner "root"
+  group "root"
+  mode "0755"
+  content "[general]\n state_file = /tmp/watchstate\n[/var/log/kern.log]\n datetime_format = %b %d %H:%M:%S\nlog_group_name = /var/log/kern.log\nfile = /var/log/kern.log\nlog_stream_name = ::hostname::"
+  action : create_if_missing
+end
