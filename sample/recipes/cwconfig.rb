51f12c01878b0ec78e6ba7d1a1bdf80b19c3d2ad
cbbarclay <cbbarclay@hotmail.com>
2014-04-18 15:59:21 -0700

    Update cwconfig.rb

--- sample/recipes/cwconfig.rb
+++ sample/recipes/cwconfig.rb
@@ -1,7 +1,15 @@
-file "/tmp/cwlogs" do
+#file "/tmp/cwlogs" do
+#  owner "root"
+#  group "root"
+#  mode "0755"
+#  content "[general]\nstate_file = /tmp/watchstate\n[/var/log/kern.log]\ndatetime_format = %b %d %H:%M:%S\nlog_group_name = /var/log/kern.log\nfile = /var/log/kern.log\nlog_stream_name = ::hostname::"
+#  action :create_if_missing
+#end
+
+template "/tmp/cwlogs.cfg" do
+  cookbook "sample"
+  source "cwlogs.cfg.erb"
   owner "root"
   group "root"
-  mode "0755"
-  content "[general]\nstate_file = /tmp/watchstate\n[/var/log/kern.log]\ndatetime_format = %b %d %H:%M:%S\nlog_group_name = /var/log/kern.log\nfile = /var/log/kern.log\nlog_stream_name = ::hostname::"
-  action :create_if_missing
+  mode 0644
 end
