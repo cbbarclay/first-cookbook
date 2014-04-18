aa33f10cc67ec474d0941bdc986eedbb56e4229f
cbbarclay <cbbarclay@hotmail.com>
2014-04-18 16:00:18 -0700

    Update cwstart.rb

--- sample/recipes/cwstart.rb
+++ sample/recipes/cwstart.rb
@@ -1,15 +1,4 @@
-package "daemon"
-
-# script file used by service to launch your java program
-file "/tmp/run_script.cmd" do
-    content "aws logs push --config-file /root/.aws/awslogs_config.cfg"
+execute "send logs to CloudWatch" do
+  command "nohup sudo aws --region us-east-1 logs push --config-file /tmp/cwlogs &"
 end
 
-cookbook_file '/etc/init.d/myservice' do
-    source 'etc_initd_myservice'
-end
-
-service "myservice" do
-    supports :restart => true, :start => true, :stop => true, :reload => true
-    action [:enable]
-end
