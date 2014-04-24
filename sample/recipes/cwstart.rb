78d01d4e15f58934edcfb92cd26eb29ce574c1a4
cbbarclay <cbbarclay@hotmail.com>
2014-04-24 11:22:33 -0700

    Update cwstart.rb

--- sample/recipes/cwstart.rb
+++ sample/recipes/cwstart.rb
@@ -1,7 +1,7 @@
 execute "install" do
   command "wget https://s3.amazonaws.com/cf8427e/Setup/awslogs-agent-setup.py"
   command "chmod u+x awslogs-agent-setup.py"
-  command "./awslogs-agent-setup.py -r us-east-1 -c /tmp/cwlogs"
+  command "python awslogs-agent-setup.py -r us-east-1 -c /tmp/cwlogs.cfg"
 
   action :run
 end
