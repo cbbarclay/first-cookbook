bc8747d7e8715944e92ae5d453a7aa3d7411cacd
cbbarclay <cbbarclay@hotmail.com>
2014-05-20 03:12:00 -0700

    Update cwstart.rb

--- sample/recipes/cwstart.rb
+++ sample/recipes/cwstart.rb
@@ -2,7 +2,7 @@ bash "start_service" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  wget https://s3.amazonaws.com/cf8427e/Setup /awslogs-agent-setup.py
+  wget https://s3.amazonaws.com/cf8427e/Setup/awslogs-agent-setup.py
   chmod u+x awslogs-agent-setup.py
   ./awslogs-agent-setup.py -r us-east-1 -c /tmp/cwlogs.cfg
   EOH
