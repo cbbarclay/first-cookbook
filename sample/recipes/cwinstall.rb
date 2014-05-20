e9e5dd38df7a539cd0940a4cb1b3d4c32dd3bca3
cbbarclay <cbbarclay@hotmail.com>
2014-05-20 01:54:59 -0700

    Update cwinstall.rb

--- sample/recipes/cwinstall.rb
+++ sample/recipes/cwinstall.rb
@@ -2,8 +2,13 @@ package "python-pip" do
     action :install
 end 
 
-execute "install" do
+execute "cli-install" do
   command "pip install --upgrade https://s3.amazonaws.com/cf8427e/awscli-1.3.0.tar.gz"
+
+  action :run
+end
+
+execute "boto-install" do
   command "pip install --upgrade https://s3.amazonaws.com/cf8427e/botocore-0.34.0.tar.gz"
 
   action :run
