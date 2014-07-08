c712221b09bdcaaf732a7bccc8912dbf20b32ed1
cbbarclay <cbbarclay@hotmail.com>
2014-07-08 13:18:04 -0700

    Update mestart.rb

--- sample/recipes/mestart.rb
+++ sample/recipes/mestart.rb
@@ -1,5 +1,4 @@
 execute "Launch CloudWatch Logs agent" do
-command "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py -n -r us-east-1 -c
-/tmp/cwlogs.cfg"
+command "/opt/aws/cloudwatch/awslogs-agent-setup-v1.0.py -n -r us-east-1 -c /tmp/cwlogs.cfg"
 not_if { system "pgrep -f aws-logs-agent-setup" }
 end
