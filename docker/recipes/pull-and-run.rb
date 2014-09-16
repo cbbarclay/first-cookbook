705824b4c04dd2b22212bb47531052d7f6d6dfe3
cbbarclay <cbbarclay@hotmail.com>
2014-09-16 15:52:33 -0700

    Create pull-and-run.rb

---
+++ docker/recipes/pull-and-run.rb
@@ -0,0 +1,19 @@
+node[:my_apps].each do |name, image|  
+  script "pull_app_#{name}_image" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+      docker pull #{image}
+    EOH
+  end
+end 
+
+node[:my_apps].each do |name, image|  
+  script "run_app_#{name}_container" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+      docker run -d --name=#{name} #{image}
+    EOH
+  end
+end
