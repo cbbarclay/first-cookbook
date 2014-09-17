638b58fed5128a17856c92b11433bb0e1be5f54e
cbbarclay <cbbarclay@hotmail.com>
2014-09-17 16:54:59 -0700

    Update docker-deploy.rb

--- docker/recipes/docker-deploy.rb
+++ docker/recipes/docker-deploy.rb
@@ -14,14 +14,14 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-#current_dir = ::File.join(deploy[:deploy_to], 'current')
+#need to grep for existing images and docker rmi imagename + docker stop name
 
   bash "docker-build" do
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
      docker build -t=#{deploy[:application]} . > #{deploy[:application]}-docker.out
-     docker run -p #{node[:opsworks][:instance][:private_ip]}:80:80 -d #{deploy[:application]}
+     docker run -p #{node[:opsworks][:instance][:private_ip]}:#{deploy[:environment_variables][:service_port]}:#{deploy[:environment_variables][:container_port]} -d #{deploy[:application]}
     EOH
   end
 
