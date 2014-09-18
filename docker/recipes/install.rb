503d009d8878e11a6bff33a61215f5dcf33b1912
cbbarclay <cbbarclay@hotmail.com>
2014-09-18 09:00:22 -0700

    Update install.rb

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -1,6 +1,13 @@
-package "docker" do  
-  action :install
-end 
+case node[:platform]
+when "ubuntu","debian"
+  package "docker.io" do
+    action :install
+  end
+when 'centos','redhat','fedora','amazon'
+  package "docker" do
+    action :install
+  end
+end
 
 service "docker" do
   action :start
