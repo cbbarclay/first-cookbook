60d91fb7f06c1ace4920e45323de6b5942b15e42
cbbarclay <cbbarclay@hotmail.com>
2014-10-17 13:50:59 -0700

    Update exec-sh.rb

--- sample/recipes/exec-sh.rb
+++ sample/recipes/exec-sh.rb
@@ -1,3 +1,20 @@
+    time = Time.now.to_i
+results = "/tmp/#{time}"
+
 execute "exec-sh" do
-    command "#{node[:script][:command]} #{node[:script][:args]}"
+    command "#{node[:script][:command]} #{node[:script][:args]} &> #{results}"
+    notifies :run, 'execute[print-it]', :delayed
 end
+
+#ruby_block "Results" do
+#    only_if { ::File.exists?(results) }
+#    block do
+#      Chef::Log.debug(File.read(results))
+#    end
+#end
+
+execute "print-it" do
+        Chef::Log.debug(File.read(results))
+action :nothing
+end
+
