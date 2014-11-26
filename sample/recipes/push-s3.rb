f638f02677cad22d23397c70bd0fb5b43cb2029d
cbbarclay <cbbarclay@hotmail.com>
2014-11-25 19:34:00 -0800

    Update push-s3.rb

--- sample/recipes/push-s3.rb
+++ sample/recipes/push-s3.rb
@@ -1,8 +1,8 @@
-getS3.rb
+#getS3.rb
 require 'rubygems'
 require 'aws-sdk'
 
 s3 = AWS::S3.new
 key = File.basename(node[:s3][:filename])
 s3.buckets[node[:s3][:bucketname]].objects[key].write(:file => node[:s3][:filepath])
-Chef::Log.info("Uploading file #{file_name} to bucket #{bucket_name}.")
+#Chef::Log.info("Uploading file #{file_name} to bucket #{bucket_name}.")
